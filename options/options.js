27e08331f12c59e6be7e25e6847799ce9da9f533
Crowdbotics <team@crowdbotics.com>
2024-01-29 18:04:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "doxo-demo-1-45665",
+  url: "https://doxo-demo-1-45665.botics.co",
+  api: "https://doxo-demo-1-45665.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
